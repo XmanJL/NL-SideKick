283d29183af48b00f80cbe9681ca99ce6091e841
Rickey H. <59082426+RickeyHo@users.noreply.github.com>
2025-04-19 12:46:29 -0700

    Add files via upload
    backend

---
+++ main.py
@@ -0,0 +1,73 @@
+from dotenv import load_dotenv
+import os
+from openai import OpenAI, api_key
+import json
+import pyttsx3
+import nltk
+import wordfreq
+import ssl
+import time
+from flask import Flask
+from flask import send_file
+import re
+from threading import Lock
+from wordfreq import word_frequency
+from pydub import AudioSegment
+from gtts import gTTS
+app = Flask(__name__)
+
+
+@app.route("/content/<originalSentence>/<wordCount>")
+def languageHelp(originalSentence, wordCount):
+
+    engine = pyttsx3.init()
+    amountDefined = int(wordCount)
+
+    inputSentence = re.sub(r'[^\w\s&&[^\']]','', originalSentence)
+    inputSentence = re.sub(r'\d','', inputSentence)
+
+    print(inputSentence)
+    tokenizedInput = nltk.word_tokenize(inputSentence)
+
+    frequencyList = []
+
+    for word in tokenizedInput:
+        frequencyList.append(word_frequency(word, 'en'))
+
+
+    hardWordsList = []
+
+    for num in range(amountDefined):
+        hardWordsList.append(tokenizedInput[frequencyList.index(min(frequencyList))])
+        frequencyList[frequencyList.index(min(frequencyList))] = 1
+
+    print(hardWordsList)
+    with open("DONOTSTAGE.json", "r") as apiLocation:
+        config = json.load(apiLocation)
+
+    client = OpenAI(
+        # This is the default and can be omitted
+        api_key=config['api_key'],
+    )
+
+    response = client.responses.create(
+        model="gpt-4o",
+        instructions="You are local friend that is helping a friend navigate your home country, you will be given words to define as short and simply as possible.",
+        input="In the context of " + originalSentence + ", please define the words" + str(hardWordsList) + "SIMPLY and SHORTEN BUT NOT TOO MUCH and IGNORE NAMES and format it as DEFINED_WORD is DEFINITION",
+    )
+
+    print(response.output_text)
+    tts = gTTS(response.output_text)
+    tts.save("output.mp3")
+
+    return send_file("output.mp3")
+
+
+
+
+
+
+
+
+
+
